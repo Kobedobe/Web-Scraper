1dd09e313635c2f5ca83ca956613be78ceaef684
Kobedobe <136351482+Kobedobe@users.noreply.github.com>
2024-01-20 16:05:44 +0000

    Update data.py

--- data.py
+++ data.py
@@ -7,7 +7,6 @@ directory = 'CSV Files'
 for filename in os.listdir(directory):
     # if 'PC' in filename:
     f = os.path.join(directory,filename)
-    print(f)
     df = pd.read_csv(f)
     df = df.drop_duplicates(keep=False, subset = ['Game'])
     df = df.fillna('N/A')
